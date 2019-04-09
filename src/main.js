138ee9dbe9662d49304792e8f8e2613767157230
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2019-04-09 20:13:25 +0200

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+let mainWindow
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  console.log()
+
+  mainWindow.loadFile(path.resolve(__dirname, `../resource/index.html`))
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
